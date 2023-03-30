29119095df8e290c730819c10510a1e9f5c08598
Samuel Jonathan <samueljonathanleumas@gmail.com>
2023-03-30 20:34:04 -0300

    30/03

--- src/main/java/com/example/exemplojpa/ExemplojpaApplication.java
+++ src/main/java/com/example/exemplojpa/ExemplojpaApplication.java
@@ -1,5 +1,7 @@
 package com.example.exemplojpa;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -15,14 +17,42 @@ public class ExemplojpaApplication {
 @Bean
 public CommandLineRunner init(@Autowired CursoRepository cursoRepository) {
 return args ->{
-	Curso c1 =new Curso((long)0, "Análise e Desenvolvimento de Sistema",250);
-	Curso c2 =new Curso((long)0, "Jogos Digitais",200);
-	Curso c3 =new Curso((long)0, "Gestão de T.I.",200);
-	Curso c4 =new Curso((long)0, "Eng. Computação",500);
+	System.out.println("**** INSERINDO CURSO ****");
+	Curso c1 =new Curso((long)1, "Análise e Desenvolvimento de Sistema",250);
+	Curso c2 =new Curso((long)2, "Jogos Digitais",200);
+	Curso c3 =new Curso((long)3, "Gestão de T.I.",200);
+	Curso c4 =new Curso((long)4, "Eng. Computação",500);
 	cursoRepository.inserir(c1);
 	cursoRepository.inserir(c2);
 	cursoRepository.inserir(c3);
 	cursoRepository.inserir(c4);
+
+	System.out.println("**** EDITANDO CURSO ****");
+	c1 = cursoRepository.obterPorId(1);
+	c1.setNome("Análise e Desenvolvimento de SistemaS");
+	cursoRepository.editar(c1);
+
+	System.out.println("**** LISTAR TODOS CURSOS ****");
+	List<Curso> cursos = cursoRepository.obterTodos();
+	for (Curso c : cursos ){
+		System.out.println(c.getNome());
+	}
+	System.out.println("**** OBTER POR NOME ****");
+	cursos = cursoRepository.obterPorNome("Análise%");
+	for (Curso c : cursos ){
+		System.out.println(c.getNome());
+	}
+
+	System.out.println("**** EXCLUIR CURSO ****");
+	cursoRepository.excluir(2);
+
+	System.out.println("**** MOSTRAR TODOS CURSOS ****");
+	cursos = cursoRepository.obterTodos();
+	for (Curso c : cursos ){
+		System.out.println(c.getNome());
+	}
+
+
 };
 }
 
--- src/main/java/com/example/exemplojpa/repository/CursoRepository.java
+++ src/main/java/com/example/exemplojpa/repository/CursoRepository.java
@@ -1,11 +1,14 @@
 package com.example.exemplojpa.repository;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.example.exemplojpa.model.Curso;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import jakarta.transaction.Transactional;
 
 @Repository
@@ -17,63 +20,35 @@ public class CursoRepository {
     public Curso inserir(Curso curso){
         return entityManager.merge(curso);
     }
+
     @Transactional
-    public void deletar(Long id) {
-        Curso curso = entityManager.find(Curso.class, id);
-        entityManager.remove(curso);
+    public Curso editar(Curso curso){
+        return entityManager.merge(curso);
     }
+    
+    @Transactional
+    public void  excluir(Curso curso){
+         entityManager.remove(curso);
+    }
+
+    @Transactional
+    public Curso obterPorId(long id){
+        return entityManager.find(Curso.class, id);
+    }
+
+    @Transactional
+    public void  excluir(long id){
+        entityManager.remove(obterPorId(id));
+    }
+
+    public List<Curso> obterTodos(){
+        return entityManager.createQuery("from Curso",Curso.class).getResultList();
+    }
+    public List<Curso> obterPorNome(String nome){
+        String jpql = "select c from Curso where nome like :nome";
+        TypedQuery<Curso> query = entityManager.createQuery(jpql,Curso.class);
+        query.setParameter("nome", nome);
+        return query.getResultList();
+    }
+    
 }
-
-
-
-<h1>Java Persistence API Curso</h1>
-
-<p>Este projeto é um exemplo de aplicação Java que utiliza o framework Spring Boot e a tecnologia Java Persistence API (JPA) para implementar um simples CRUD (Create, Read, Update, Delete) para um modelo de dados de cursos.</p>
-
-<h2>Pré-requisitos</h2>
-
-<ul>
-	<li>Java 8 ou superior</li>
-	<li>Maven 3 ou superior</li>
-	<li>Algum editor de código, como o Eclipse ou o IntelliJ IDEA</li>
-</ul>
-
-<h2>Instalação</h2>
-
-<p>Para instalar e executar este projeto, siga as instruções abaixo:</p>
-
-<ol>
-	<li>Clone este repositório em sua máquina local</li>
-	<li>Abra um terminal na pasta raiz do projeto</li>
-	<li>Execute o comando <code>mvn clean install</code> para compilar o projeto e gerar o pacote executável</li>
-	<li>Execute o comando <code>java -jar target/exemplojpa-0.0.1-SNAPSHOT.jar</code> para iniciar a aplicação</li>
-	<li>Abra um navegador web e acesse a URL <code>http://localhost:8080/cursos</code> para visualizar a lista de cursos</li>
-</ol>
-
-<h2>Tecnologias utilizadas</h2>
-
-<ul>
-	<li>Spring Boot - framework para criar aplicações web em Java</li>
-	<li>Java Persistence API (JPA) - tecnologia para mapeamento objeto-relacional em Java</li>
-	<li>Maven - ferramenta para gerenciamento de dependências e compilação de projetos Java</li>
-	<li>H2 Database - banco de dados em memória utilizado para armazenar os dados da aplicação</li>
-	<li>Thymeleaf - motor de template utilizado para renderizar as páginas web da aplicação</li>
-</ul>
-
-<h2>Funcionalidades</h2>
-
-<ul>
-	<li>Listagem de cursos - página que exibe uma tabela com os cursos cadastrados no sistema</li>
-	<li>Cadastro de curso - página que permite ao usuário cadastrar um novo curso no sistema</li>
-	<li>Atualização de curso - página que permite ao usuário atualizar um curso existente no sistema</li>
-	<li>Exclusão de curso - página que permite ao usuário excluir um curso existente no sistema</li>
-</ul>
-
-<h2>Autor</h2>
-
-<p>Este projeto foi criado por [seu nome aqui].</p>
-
-<h2>Licença</h2>
-
-<p>Este projeto está licenciado sob a licença MIT. Consulte o arquivo LICENSE para obter mais informações.</p>
-
